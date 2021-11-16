8d373e0e9ab1239ea1825a590e4c30705f58ab66
Nutin208 <nutdanai.282@mail.kmutt.ac.th>
2021-11-16 23:16:24 +0700

    Auto stash before merge of "main" and "origin/main"

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from typing import final
 from flask import Flask, render_template, request
 import keras
 import tensorflow as tf
@@ -8,33 +9,63 @@ import numpy as np
 
 
 app = Flask(__name__)
-
-model = load_model('modelNut.h5')
-reconstructed_model = keras.models.load_model("modelNut.h5")
+ispapaya_model = load_model('isPapayaModel.h5')
+ispapaya_reconstructed_model = keras.models.load_model("isPapayaModel.h5")
+ripeness_model = load_model('ripenessModel.h5')
+ripeness_reconstructed_model = keras.models.load_model("ripenessModel.h5")
 batch_size = 32
 img_height = 64
 img_width = 64
-class_names=['Medium', 'Not_Ripe', 'Ripe']
+ripeness_class_names=['Medium', 'Not_Ripe', 'Ripe']
+ispapaya_class_names=['Not Papaya','Papaya']
 
-def predict_label(img_path):
+def final_predict(img_path):
+	final_ans  = isPapaya_predict_label(img_path)
+	if(final_ans[0]=='Papaya'):
+		final_ans  = ripeness_predict_label(img_path)
+	return final_ans
+
+def ripeness_predict_label(img_path):
 	i = image.load_img(img_path, target_size=(img_height,img_width))
 	i = image.img_to_array(i)/255.0
 	i = i.reshape(1, 64,64,3)
-	p = model.predict(i)
+	p = ripeness_model.predict(i)
 	img = keras.preprocessing.image.load_img(
     img_path, target_size=(img_height, img_width)
 )
 	img_array = keras.preprocessing.image.img_to_array(img)
 	img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-	predictions = reconstructed_model.predict(img_array)
+	predictions = ripeness_reconstructed_model.predict(img_array)
 	score = tf.nn.softmax(predictions[0])
 
 	print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
+    .format(ripeness_class_names[np.argmax(score)], 100 * np.max(score))
 	)
-	return [class_names[np.argmax(score)], str(round(100 * np.max(score), 2)) ]
+	return [ripeness_class_names[np.argmax(score)], str(round(100 * np.max(score), 2)) ]
+
+def isPapaya_predict_label(img_path):
+	i = image.load_img(img_path, target_size=(img_height,img_width))
+	i = image.img_to_array(i)/255.0
+	i = i.reshape(1, 64,64,3)
+	p = ispapaya_model.predict(i)
+	img = keras.preprocessing.image.load_img(
+    img_path, target_size=(img_height, img_width)
+)
+	img_array = keras.preprocessing.image.img_to_array(img)
+	img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+	predictions = ispapaya_reconstructed_model.predict(img_array)
+
+	score = tf.nn.softmax(predictions[0])
+
+	print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(ispapaya_class_names[np.argmax(score)], 100 * np.max(score))
+	)
+		
+	return [ispapaya_class_names[np.argmax(score)], str(round(100 * np.max(score), 2)) ]
 
 @app.route('/',methods = ['GET', 'POST'])
 def home():
@@ -46,9 +77,10 @@ def get_output():
 		img = request.files['my_image']
 
 		img_path = "static/" + img.filename	
+		# img_path = "static/test.png"
 		img.save(img_path)
 
-		p = predict_label(img_path)
+		p = final_predict(img_path)
 
 	return render_template("index.html", prediction = p, img_path = img_path)
 
